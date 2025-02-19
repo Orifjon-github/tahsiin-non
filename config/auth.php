e7af2a799729bb9f7c9fad6d5c48135278a3e7e8
Orifjon-github <avazbekoripov94@gmail.com>
2025-02-19 16:38:33 +0500

    test

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,41 @@
 <?php
 
 return [
+    'defaults' => [
+        'guard' => 'web',
+        'passwords' => 'users',
+    ],
+
+    'guards' => [
+        'web' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+    ],
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\User::class,
+        ],
+    ],
+
+    'passwords' => [
+        'users' => [
+            'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+    ],
+
+    'password_timeout' => 10800,
     'clients' => [
         env('TOKEN_API_V2') => 'api.v2',
         env('TOKEN_API_V3') => 'api.v3',
