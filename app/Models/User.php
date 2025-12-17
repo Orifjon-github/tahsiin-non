f2fd8ff58e6a5803b3e0d3cae19fe05690f02fcf
Orifjon-github <avazbekoripov94@gmail.com>
2025-12-17 16:54:12 +0500

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
         'is_active' => 'boolean',
+        'from_qr' => 'boolean',
     ];
     /**
      * Foydalanuvchining buyurtmalari
